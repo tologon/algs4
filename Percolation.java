bb1386b95978ea39e56aa7461ceb82ce4c22f626
tologon <teshimkanov@gmail.com>
2015-09-14 23:09:06 -0400

    Fix constructor & add method to transform coordinates to index.

--- Percolation.java
+++ Percolation.java
@@ -2,24 +2,28 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 import edu.princeton.cs.algs4.StdOut;
 
 public class Percolation {
-  // formula for transforming row i & column j to WQUF index
-  // N * (i - 1) + j - 1
-
   private WeightedQuickUnionUF uf; // union-find data structure
   private boolean[] sites; // temporal use to track closed/open/full sites
   private int initialN;
 
   // create N-by-N grid, with all sites blocked
   public Percolation(int N) {
+    if (N <= 0) throw new java.lang.IllegalArgumentException();
     initialN = N;
     uf = new WeightedQuickUnionUF(initialN * initialN + 2);
     sites = new boolean[initialN * initialN + 2];
+    // set virtual top & bottom to be open sites
+    sites[initialN * initialN] = true;
+    sites[initialN * initialN + 1] true;
   }
 
   // public void open(int i, int j) // open site (row i, column j) if it is not open already
   // public boolean isOpen(int i, int j) // is site (row i, column j) open?
   // public boolean isFull(int i, int j) // is site (row i, column j) full?
   // public boolean percolates() // does the system percolate?
+
+  private coordinatesToIndex(int i, int j)
+  { return initialN * (i - 1) + j - 1; }
 
   // test client (optional)
   public static void main(String[] args) {
