741bb35f4685e47d434d29c1388ea70e275e8381
tologon <teshimkanov@gmail.com>
2015-09-14 22:58:48 -0400

    Add constructor & simple test for it.

--- Percolation.java
+++ Percolation.java
@@ -1,9 +1,31 @@
-public class Percolation {
-  public Percolation(int N) // create N-by-N grid, with all sites blocked
-  public void open(int i, int j) // open site (row i, column j) if it is not open already
-  public boolean isOpen(int i, int j) // is site (row i, column j) open?
-  public boolean isFull(int i, int j) // is site (row i, column j) full?
-  public boolean percolates() // does the system percolate?
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+import edu.princeton.cs.algs4.StdOut;
 
-  public static void main(String[] args) // test client (optional)
+public class Percolation {
+  // formula for transforming row i & column j to WQUF index
+  // N * (i - 1) + j - 1
+
+  private WeightedQuickUnionUF uf; // union-find data structure
+  private boolean[] sites; // temporal use to track closed/open/full sites
+  private int initialN;
+
+  // create N-by-N grid, with all sites blocked
+  public Percolation(int N) {
+    initialN = N;
+    uf = new WeightedQuickUnionUF(initialN * initialN + 2);
+    sites = new boolean[initialN * initialN + 2];
+  }
+
+  // public void open(int i, int j) // open site (row i, column j) if it is not open already
+  // public boolean isOpen(int i, int j) // is site (row i, column j) open?
+  // public boolean isFull(int i, int j) // is site (row i, column j) full?
+  // public boolean percolates() // does the system percolate?
+
+  // test client (optional)
+  public static void main(String[] args) {
+    StdOut.println("Testing the constructor of Percolation class.");
+    StdOut.print("Creating a Percolation object with 9 sites... ");
+    Percolation p = new Percolation(3);
+    StdOut.println("The object is successfully created.");
+  }
 }
