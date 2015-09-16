b1eb11c251ed4def4e85bc8e4c7cc785b929bb70
tologon <teshimkanov@gmail.com>
2015-09-16 00:01:08 -0400

    Refactor constructor & add percolates() method.

--- Percolation.java
+++ Percolation.java
@@ -4,7 +4,8 @@ import edu.princeton.cs.algs4.StdOut;
 public class Percolation {
   private WeightedQuickUnionUF uf; // union-find data structure
   private boolean[] sites; // temporal use to track closed/open/full sites
-  private int initialN;
+  private int initialN; // useful for various calculations & initializations
+  private int top, bottom; // virtual top & bottom sites (index goes 0 to N-1)
 
   // create N-by-N grid, with all sites blocked
   public Percolation(int N) {
@@ -12,17 +13,22 @@ public class Percolation {
     initialN = N;
     uf = new WeightedQuickUnionUF(initialN * initialN + 2);
     sites = new boolean[initialN * initialN + 2];
+    top = initialN * initialN;
+    bottom = top + 1;
     // set virtual top & bottom to be open sites
-    sites[initialN * initialN] = true;
-    sites[initialN * initialN + 1] true;
+    sites[top] = true;
+    sites[bottom] = true;
   }
 
   // public void open(int i, int j) // open site (row i, column j) if it is not open already
   // public boolean isOpen(int i, int j) // is site (row i, column j) open?
   // public boolean isFull(int i, int j) // is site (row i, column j) full?
-  // public boolean percolates() // does the system percolate?
 
-  private coordinatesToIndex(int i, int j)
+  // does the system percolate?
+  public boolean percolates()
+  { return uf.connected(top, bottom); }
+
+  private int coordinatesToIndex(int i, int j)
   { return initialN * (i - 1) + j - 1; }
 
   // test client (optional)
@@ -31,5 +37,7 @@ public class Percolation {
     StdOut.print("Creating a Percolation object with 9 sites... ");
     Percolation p = new Percolation(3);
     StdOut.println("The object is successfully created.");
+
+    StdOut.println("Does the system percolate?: " + p.percolates());
   }
 }
