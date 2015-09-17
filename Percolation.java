80d92f4361b569102fce04dff0ed50796635fe6f
Tologon Eshimkanov <teshimkanov@gmail.com>
2015-09-16 21:29:49 -0400

    Add (possibly working) implementation for most methods in Percolation.

--- Percolation.java
+++ Percolation.java
@@ -5,7 +5,7 @@ public class Percolation {
   private WeightedQuickUnionUF uf; // union-find data structure
   private boolean[] sites; // temporal use to track closed/open/full sites
   private int initialN; // useful for various calculations & initializations
-  private int top, bottom; // virtual top & bottom sites (index goes 0 to N-1)
+  private int topSite, bottomSite; // virtual top & bottom sites (index goes 0 to N-1)
 
   // create N-by-N grid, with all sites blocked
   public Percolation(int N) {
@@ -13,20 +13,65 @@ public class Percolation {
     initialN = N;
     uf = new WeightedQuickUnionUF(initialN * initialN + 2);
     sites = new boolean[initialN * initialN + 2];
-    top = initialN * initialN;
-    bottom = top + 1;
+    topSite = initialN * initialN;
+    bottomSite = topSite + 1;
     // set virtual top & bottom to be open sites
-    sites[top] = true;
-    sites[bottom] = true;
+    sites[topSite] = true;
+    sites[bottomSite] = true;
   }
 
-  // public void open(int i, int j) // open site (row i, column j) if it is not open already
-  // public boolean isOpen(int i, int j) // is site (row i, column j) open?
-  // public boolean isFull(int i, int j) // is site (row i, column j) full?
+  // open site (row i, column j) if it is not open already
+  public void open(int i, int j) {
+    if (isOpen(int i, int j)) return;
+    int aSite = coordinatesToIndex(i, j);
+    sites[aSite] = true;
+    // assuming i is a valid index,
+    // if it's in a top row, connect it to virtual top site
+    if (i == 0)
+    { uf.connect(topSite, aSite); }
+    // if i is in a bottom row, connect it to virtual bottom site
+    else if (i == initialN - 1)
+    { uf.connect(bottomSite, aSite); }
+
+    // connect with all valid & open neighbors
+
+    // connect w/ north neighbor
+    connectNeighbors(i - 1, j, aSite);
+    // connect w/ south neighbor
+    connectNeighbors(i + 1, j, aSite);
+    // connect w/ east neighbor
+    connectNeighbors(i, j + 1, aSite);
+    // connect w/ west neighbor
+    connectNeighbors(i, j - 1, aSite);
+  }
+
+  // assuming that a site at row i & column j is open
+  private void connectNeighbors(int i, int j, int aSite) {
+    // connect with a given neighbor
+    if (isValid(i, j) && isOpen(i, j)) {
+      int neighborSite = coordinatesToIndexi, j);
+      uf.connect(aSite, neighborSite);
+    }
+  }
+
+  // checks whether row i and column j are valid
+  // private boolean isValid(int i, int j)
+
+  // is site (row i, column j) open?
+  public boolean isOpen(int i, int j) {
+    int aSite = coordinatesToIndex(i, j);
+    return sites[aSite];
+  }
+
+  // is site (row i, column j) full?
+  public boolean isFull(int i, int j) {
+    int aSite = coordinatesToIndex(i, j);
+    return uf.connected(topSite, aSite);
+  }
 
   // does the system percolate?
   public boolean percolates()
-  { return uf.connected(top, bottom); }
+  { return uf.connected(topSite, bottomSite); }
 
   private int coordinatesToIndex(int i, int j)
   { return initialN * (i - 1) + j - 1; }
