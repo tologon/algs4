3463dfaa44a057a4acc686b9100b1e5e692141d1
Tologon Eshimkanov <teshimkanov@gmail.com>
2015-09-18 08:16:49 -0400

    Attempt to implement constructor for Stats class.

--- 1_percolation/PercolationStats.java
+++ 1_percolation/PercolationStats.java
@@ -1,6 +1,26 @@
 public class PercolationStats {
+  private Percolation p;
+  private int initialN;
+  private int initialT;
+  private double[] experiments;
+
   // perform T independent experiments on an N-by-N grid
-  public PercolationStats(int N, int T)
+  public PercolationStats(int N, int T) {
+    initialN = N;
+    initialT = T;
+    int openSites;
+    for (int i = 0; i < initialT; i++) {
+      p = new Percolation(N);
+      openSites = 0;
+      while (!p.percolates()) {
+        int randomRow = StdRandom.uniform(initialN);
+        int randomColumn = StdRandom.uniform(initialN);
+        if (!p.isOpen(randomRow, randomColumn)) openSites++;
+        p.open(randomRow, randomColumn);
+      }
+      experiments[i] = openSites;
+    }
+  }
 
   // sample mean of percolation threshold
   public double mean()
@@ -16,6 +36,6 @@ public class PercolationStats {
 
   // test client
   public static void main(String[] args) {
-    
+
   }
 }
