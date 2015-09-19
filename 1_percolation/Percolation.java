5e1c2cf0890a86106be4bb41c5be30622c8a0ba2
Tologon Eshimkanov <teshimkanov@gmail.com>
2015-09-19 17:55:40 -0400

    Add stats methods for PercolationStats class.

--- 1_percolation/Percolation.java
+++ 1_percolation/Percolation.java
@@ -9,7 +9,7 @@ public class Percolation {
 
   // create N-by-N grid, with all sites blocked
   public Percolation(int N) {
-    if (N <= 0) throw new java.lang.IllegalArgumentException();
+    if (N <= 0) throw new IllegalArgumentException();
     initialN = N;
     uf = new WeightedQuickUnionUF(initialN * initialN + 2);
     sites = new boolean[initialN * initialN + 2];
@@ -62,14 +62,14 @@ public class Percolation {
 
   // is site (row i, column j) open?
   public boolean isOpen(int i, int j) {
-    if (!isValid(i, j)) throw new java.lang.IndexOutOfBoundsException();
+    if (!isValid(i, j)) throw new IndexOutOfBoundsException();
     int aSite = coordinatesToIndex(i, j);
     return sites[aSite];
   }
 
   // is site (row i, column j) full?
   public boolean isFull(int i, int j) {
-    if (!isValid(i, j)) throw new java.lang.IndexOutOfBoundsException();
+    if (!isValid(i, j)) throw new IndexOutOfBoundsException();
     int aSite = coordinatesToIndex(i, j);
     return uf.connected(topSite, aSite);
   }
--- 1_percolation/PercolationStats.java
+++ 1_percolation/PercolationStats.java
@@ -1,51 +1,59 @@
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.In;
 
 public class PercolationStats {
-  private Percolation p;
-  private int initialN;
-  private int initialT;
-  private double[] experiments;
+  private static Percolation p;
+  private static int numOfSites;
+  private static int initialT;
+  private static double[] thresholds;
 
   // perform T independent experiments on an N-by-N grid
   public PercolationStats(int N, int T) {
-    initialN = N;
+    numOfSites = N * N;
     initialT = T;
     int openSites;
-    experiments = new double[initialT];
+    thresholds = new double[initialT];
     for (int i = 0; i < initialT; i++) {
       p = new Percolation(N);
       openSites = 0;
       while (!p.percolates()) {
-        int randomRow = StdRandom.uniform(1, initialN + 1);
-        int randomColumn = StdRandom.uniform(1, initialN + 1);
-        // StdOut.println("i: " + randomRow
-        //             +  "; j: " + randomColumn);
+        int randomRow = StdRandom.uniform(1, N + 1);
+        int randomColumn = StdRandom.uniform(1, N + 1);
         if (!p.isOpen(randomRow, randomColumn)) {
           openSites++;
           p.open(randomRow, randomColumn);
         }
       }
-      experiments[i] = openSites;
+      thresholds[i] = (double) openSites / numOfSites;
     }
   }
 
   // sample mean of percolation threshold
-  // public double mean()
+  public double mean()
+  { return StdStats.mean(thresholds); }
 
   // sample standard deviation of percolation threshold
-  // public double stddev()
+  public double stddev()
+  { return StdStats.stddev(thresholds); }
 
   // low endpoint of 95% confidence interval
-  // public double confidenceLo()
+  public double confidenceLo()
+  { return mean() - (1.96 * stddev() / Math.sqrt(initialT)); }
 
   // high endpoint of 95% confidence interval
-  // public double confidenceHi()
+  public double confidenceHi()
+  { return mean() + (1.96 * stddev() / Math.sqrt(initialT)); }
 
   // test client
   public static void main(String[] args) {
     StdOut.print("Creating a PercolationStats object...");
-    PercolationStats ps = new PercolationStats(3, 10);
+    PercolationStats ps = new PercolationStats(20, 10);
     StdOut.println("Object successfully created.");
+    for (int i = 0; i < initialT; i++) {
+      double t = thresholds[i];
+      StdOut.println(i + "th threshold: " + t);
+    }
   }
 }
