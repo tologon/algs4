de1f758e30a89f630b83ef38ced825f25e9c51e8
Tologon Eshimkanov <teshimkanov@gmail.com>
2015-09-19 10:10:21 -0400

    Fix constructor; works as expected.

--- 1_percolation/PercolationStats.java
+++ 1_percolation/PercolationStats.java
@@ -1,3 +1,6 @@
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
 public class PercolationStats {
   private Percolation p;
   private int initialN;
@@ -9,33 +12,40 @@ public class PercolationStats {
     initialN = N;
     initialT = T;
     int openSites;
+    experiments = new double[initialT];
     for (int i = 0; i < initialT; i++) {
       p = new Percolation(N);
       openSites = 0;
       while (!p.percolates()) {
-        int randomRow = StdRandom.uniform(initialN);
-        int randomColumn = StdRandom.uniform(initialN);
-        if (!p.isOpen(randomRow, randomColumn)) openSites++;
-        p.open(randomRow, randomColumn);
+        int randomRow = StdRandom.uniform(1, initialN + 1);
+        int randomColumn = StdRandom.uniform(1, initialN + 1);
+        // StdOut.println("i: " + randomRow
+        //             +  "; j: " + randomColumn);
+        if (!p.isOpen(randomRow, randomColumn)) {
+          openSites++;
+          p.open(randomRow, randomColumn);
+        }
       }
       experiments[i] = openSites;
     }
   }
 
   // sample mean of percolation threshold
-  public double mean()
+  // public double mean()
 
   // sample standard deviation of percolation threshold
-  public double stddev()
+  // public double stddev()
 
   // low endpoint of 95% confidence interval
-  public double confidenceLo()
+  // public double confidenceLo()
 
   // high endpoint of 95% confidence interval
-  public double confidenceHi()
+  // public double confidenceHi()
 
   // test client
   public static void main(String[] args) {
-
+    StdOut.print("Creating a PercolationStats object...");
+    PercolationStats ps = new PercolationStats(3, 10);
+    StdOut.println("Object successfully created.");
   }
 }
