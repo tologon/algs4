50111d49ada7ee673e8cbe8008a7d80a3ae90ed2
Tologon Eshimkanov <teshimkanov@gmail.com>
2015-09-21 23:13:46 -0400

    Implement Deque's constructor, basic methods and add functionality.

--- 2_queues-dequeues/Deque.java
+++ 2_queues-dequeues/Deque.java
@@ -1,18 +1,48 @@
 public class Deque<Item> implements Iterable<Item> {
+  private Node first;
+  private Node last;
+  private int numOfItems;
+
+  private class Node {
+    Item item;
+    Node next;
+  }
+
   // construct an empty deque
-  public Deque()
+  public Deque() {
+    first = null;
+    last = null;
+    numOfItems = 0;
+  }
 
   // is the deque empty?
   public boolean isEmpty()
+  { return numOfItems == 0; }
 
   // return the number of items on the deque
   public int size()
+  { return numOfItems; }
 
   // add the item to the front
-  public void addFirst(Item item)
+  public void addFirst(Item item) {
+    Node oldFirst = first;
+    first = new Node();
+    first.item = item;
+    first.next = oldFirst;
+    if (isEmpty()) last = first;
+    numOfItems++;
+  }
 
   // add the item to the end
-  public void addLast(Item item)
+  public void addLast(Item item) {
+    Node oldLast = last;
+    last = new Node();
+    last.item = item;
+    last.next = null;
+    if (isEmpty()) first = last;
+    else           oldLast.next = last;
+    numOfItems++;
+  }
 
   // remove and return the item from the front
   public Item removeFirst()
