33c2d9bdc40b2a2a84b46e601ca2ae77125d6c62
Tologon Eshimkanov <teshimkanov@gmail.com>
2015-09-29 18:49:52 -0400

    Write pseudocode & implement simple methods for Brute Force.

--- 3_collinear_points/BruteCollinearPoints.java
+++ 3_collinear_points/BruteCollinearPoints.java
@@ -1,10 +1,22 @@
 public class BruteCollinearPoints {
+  private LineSegment[] segments;
+
   // finds all line segments containing 4 points
-  public BruteCollinearPoints(Point[] points)
+  public BruteCollinearPoints(Point[] points) {
+    /*
+     * for sorting, use java.util.Arrays.sort
+     * 1. for every combination of 4 points
+     * 1.1 check every pair of points on whether they are collinear or not
+     * 2. for every 4 collinear points, find end points
+     * 3. make a line segment and add it to the array of segments
+     */
+  }
 
   // the number of line segments
   public int numberOfSegments()
+  { return segments.length; }
 
   // the line segments
   public LineSegment[] segments()
+  { return segments; }
 }
