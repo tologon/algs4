ac351dbe76c1b56efe6742de2422da0d582e81ee
Tologon Eshimkanov <teshimkanov@gmail.com>
2015-09-30 19:00:54 -0400

    Start Brute Force algorithm implementation.

--- 3_collinear_points/BruteCollinearPoints.java
+++ 3_collinear_points/BruteCollinearPoints.java
@@ -1,5 +1,6 @@
 public class BruteCollinearPoints {
   private LineSegment[] segments;
+  private int count;
 
   // finds all line segments containing 4 points
   public BruteCollinearPoints(Point[] points) {
@@ -10,11 +11,31 @@ public class BruteCollinearPoints {
      * 2. for every 4 collinear points, find end points
      * 3. make a line segment and add it to the array of segments
      */
+
+     segments = new LineSegment[1];
+     count = 0;
+     for (int i = 0; i < points.length; i++) {
+       Point p1 = points[i];
+       for (int j = i + 1; j < points.length; j++) {
+         Point p2 = points[j];
+         for (int k = j + 1; k < points.length; k++) {
+           Point p3 = points[k];
+           // if 3 points are not degenerate &&
+           // their slopes match -> continue
+           // else -> break
+           for (int m = k + 1)
+         }
+       }
+     }
+  }
+
+  private void resizeArray() {
+
   }
 
   // the number of line segments
   public int numberOfSegments()
-  { return segments.length; }
+  { return count; }
 
   // the line segments
   public LineSegment[] segments()
