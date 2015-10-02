3d00c1f51484d8b0e86df24a00a3864d652ab045
Tologon Eshimkanov <teshimkanov@gmail.com>
2015-10-02 07:55:08 -0400

    Add unit testing for Brute Force algorithm.

--- 3_collinear_points/BruteCollinearPoints.java
+++ 3_collinear_points/BruteCollinearPoints.java
@@ -32,6 +32,10 @@ public class BruteCollinearPoints {
                  Point last = fourPoints[3];
                  LineSegment segment = new LineSegment(first, last);
                  segments[count++] = segment;
+                 // jump to next point (since previous are collinear)
+                 m = points.length;
+                 k = points.length;
+                 n = points.length;
                }
              }
            }
@@ -77,32 +81,33 @@ public class BruteCollinearPoints {
   public LineSegment[] segments()
   { return segments; }
 
-  public static void main(String[] args) {
-
-      // read the N points from a file
-      In in = new In(args[0]);
-      int N = in.readInt();
-      Point[] points = new Point[N];
-      for (int i = 0; i < N; i++) {
-          int x = in.readInt();
-          int y = in.readInt();
-          points[i] = new Point(x, y);
-      }
-
-      // draw the points
-      StdDraw.show(0);
-      StdDraw.setXscale(0, 32768);
-      StdDraw.setYscale(0, 32768);
-      for (Point p : points) {
-          p.draw();
-      }
-      StdDraw.show();
-
-      // print and draw the line segments
-      BruteCollinearPoints collinear = new BruteCollinearPoints(points);
-      for (LineSegment segment : collinear.segments()) {
-          StdOut.println(segment);
-          segment.draw();
-      }
-  }
+  // unit testing
+  // public static void main(String[] args) {
+  //
+  //     // read the N points from a file
+  //     In in = new In(args[0]);
+  //     int N = in.readInt();
+  //     Point[] points = new Point[N];
+  //     for (int i = 0; i < N; i++) {
+  //         int x = in.readInt();
+  //         int y = in.readInt();
+  //         points[i] = new Point(x, y);
+  //     }
+  //
+  //     // draw the points
+  //     StdDraw.show(0);
+  //     StdDraw.setXscale(0, 32768);
+  //     StdDraw.setYscale(0, 32768);
+  //     for (Point p : points) {
+  //         p.draw();
+  //     }
+  //     StdDraw.show();
+  //
+  //     // print and draw the line segments
+  //     BruteCollinearPoints collinear = new BruteCollinearPoints(points);
+  //     for (LineSegment segment : collinear.segments()) {
+  //         StdOut.println(segment);
+  //         segment.draw();
+  //     }
+  // }
 }
