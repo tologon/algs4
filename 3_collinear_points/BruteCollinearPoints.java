ef910d9f78f0ce70a8f6ba734dc155a20636a722
Tologon Eshimkanov <teshimkanov@gmail.com>
2015-10-02 19:53:22 -0400

    Account for corner cases in Brute Force algorithm.

--- 3_collinear_points/BruteCollinearPoints.java
+++ 3_collinear_points/BruteCollinearPoints.java
@@ -8,41 +8,55 @@ public class BruteCollinearPoints {
 
   // finds all line segments containing 4 points
   public BruteCollinearPoints(Point[] points) {
-     segments = new LineSegment[1];
-     count = 0;
-     for (int i = 0; i < points.length; i++) {
-       Point p1 = points[i];
-       for (int n = i + 1; n < points.length; n++) {
-         Point p2 = points[n];
-         for (int k = n + 1; k < points.length; k++) {
-           Point p3 = points[k];
-           double slope1to2 = p1.slopeTo(p2);
-           double slope1to3 = p1.slopeTo(p3);
-           double slope2to3 = p2.slopeTo(p3);
-           // 3 points are not degenerate & their slopes match
-           if (threeMatch(slope1to2, slope1to3, slope2to3)) {
-             for (int m = k + 1; m < points.length; m++) {
-               Point p4 = points[m];
-               double lastSlope = p4.slopeTo(p1);
-               if (fourMatch(slope1to2, lastSlope)) {
-                 upSize();
-                 Point[] fourPoints = {p1, p2, p3, p4};
-                 java.util.Arrays.sort(fourPoints);
-                 Point first = fourPoints[0];
-                 Point last = fourPoints[3];
-                 LineSegment segment = new LineSegment(first, last);
-                 segments[count++] = segment;
-                 // jump to next point (since previous are collinear)
-                 m = points.length;
-                 k = points.length;
-                 n = points.length;
-               }
-             }
-           }
-         }
-       }
-     }
-     downSize();
+    checkPoints(points);
+    segments = new LineSegment[1];
+    count = 0;
+    for (int i = 0; i < points.length; i++) {
+      Point p1 = points[i];
+      for (int n = i + 1; n < points.length; n++) {
+        Point p2 = points[n];
+        for (int k = n + 1; k < points.length; k++) {
+          Point p3 = points[k];
+          double slope1to2 = p1.slopeTo(p2);
+          double slope1to3 = p1.slopeTo(p3);
+          double slope2to3 = p2.slopeTo(p3);
+          // 3 points are not degenerate & their slopes match
+          if (threeMatch(slope1to2, slope1to3, slope2to3)) {
+            for (int m = k + 1; m < points.length; m++) {
+              Point p4 = points[m];
+              double lastSlope = p4.slopeTo(p1);
+              if (fourMatch(slope1to2, lastSlope)) {
+                upSize();
+                Point[] fourPoints = {p1, p2, p3, p4};
+                java.util.Arrays.sort(fourPoints);
+                Point first = fourPoints[0];
+                Point last = fourPoints[3];
+                LineSegment segment = new LineSegment(first, last);
+                segments[count++] = segment;
+                // jump to next point (since previous are collinear)
+                m = points.length;
+                k = points.length;
+                n = points.length;
+              }
+            }
+          }
+        }
+      }
+    }
+    downSize();
+  }
+
+  private void checkPoints(Point[] points) {
+    if (points == null) throw new NullPointerException();
+    for (int i = 0; i < points.length; i++) {
+      if (points[i] == null) throw new NullPointerException();
+      Point p1 = points[i];
+      for (int k = i + 1; k < points.length; k++) {
+        if (points[k] == null) throw new NullPointerException();
+        Point p2 = points[k];
+        if (p1.compareTo(p2) == 0) throw new IllegalArgumentException();
+      }
+    }
   }
 
   private boolean threeMatch(double slope1, double slope2, double slope3) {
