2aad4753a2b6c37e10a03ebb937961e77095c517
Tologon Eshimkanov <teshimkanov@gmail.com>
2015-10-03 14:54:36 -0400

    Implement updated checkPoints() for Fast algorithm.

--- 3_collinear_points/FastCollinearPoints.java
+++ 3_collinear_points/FastCollinearPoints.java
@@ -1,10 +1,30 @@
 public class FastCollinearPoints {
+  private LineSegment[] segments;
+  private int count;
+
   // finds all line segments containing 4 or more points
-  public FastCollinearPoints(Point[] points)
+  public FastCollinearPoints(Point[] points) {
+    checkPoints(points);
+  }
+
+  private void checkPoints(Point[] points) {
+    if (points == null) throw new NullPointerException();
+    if (points[0] == null) throw new NullPointerException();
+    for (int i = 0; i < points.length; i++) {
+      Point p1 = points[i];
+      for (int k = i + 1; k < points.length; k++) {
+        if (points[k] == null) throw new NullPointerException();
+        Point p2 = points[k];
+        if (p1.compareTo(p2) == 0) throw new IllegalArgumentException();
+      }
+    }
+  }
 
   // the number of line segments
   public int numberOfSegments()
+  { return count; }
 
   // the line segments
   public LineSegment[] segments()
+  { return segments; }
 }
