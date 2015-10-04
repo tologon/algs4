8e9d7fd6c99bbb0cb09857318e768a2ef5e8398a
Tologon Eshimkanov <teshimkanov@gmail.com>
2015-10-04 19:05:44 -0400

    Remove unit tests & refactor Fast algorithm.

--- 3_collinear_points/FastCollinearPoints.java
+++ 3_collinear_points/FastCollinearPoints.java
@@ -29,25 +29,32 @@ public class FastCollinearPoints {
       int numOfPoints = 1;
 
       // iterate over the rest of points & find line segments
-      for (int k = i + 1; k < points.length; k++) {
-        double newSlope = currentPoint.slopeTo(points[k]);
-        // continue until a truly "new" slope is found
-        if (currentSlope == newSlope)
-        { numOfPoints++; }
-        else {
-          // if 4 or more points, they are collinear
-          if (numOfPoints >= 4) {
-            // TODO implement upSize from Brute Force algorithm
-            Point[] collinearPoints = java.util.Arrays.copyOfRange(points, i, k + 1);
-            java.util.Arrays.sort(collinearPoints);
-            Point first = collinearPoints[0];
-            Point last = collinearPoints[collinearPoints.length - 1];
-            LineSegment s = new LineSegment(first, last);
-            segments[count++] = s;
-          }
-          currentSlope = newSlope;
-          numOfPoints = 1;
+      findLineSegments(points, currentPoint, i, currentSlope, numOfPoints);
+    }
+    downSize();
+  }
+
+  private void findLineSegments(Point[] points, Point currentPoint, int index,
+                                double slope, int numOfPoints) {
+    for (int k = index + 1; k < points.length; k++) {
+      double newSlope = currentPoint.slopeTo(points[k]);
+      // continue until a truly "new" slope is found
+      if (slope == newSlope)
+      { numOfPoints++; }
+      else {
+        // if 4 or more points, they are collinear
+        if (numOfPoints >= 4) {
+          upSize();
+          Point[] collinearPoints;
+          collinearPoints = java.util.Arrays.copyOfRange(points, index, k + 1);
+          java.util.Arrays.sort(collinearPoints);
+          Point first = collinearPoints[0];
+          Point last = collinearPoints[collinearPoints.length - 1];
+          LineSegment s = new LineSegment(first, last);
+          segments[count++] = s;
         }
+        slope = newSlope;
+        numOfPoints = 1;
       }
     }
   }
@@ -65,6 +72,24 @@ public class FastCollinearPoints {
     }
   }
 
+  private void upSize() {
+    if (count == segments.length) {
+      LineSegment[] tmp = new LineSegment[segments.length * 2];
+      for (int i = 0; i < count; i++)
+      { tmp[i] = segments[i]; }
+      segments = tmp;
+    }
+  }
+
+  private void downSize() {
+    if (count < segments.length) {
+      LineSegment[] tmp = new LineSegment[count];
+      for (int i = 0; i < count; i++)
+      { tmp[i] = segments[i]; }
+      segments = tmp;
+    }
+  }
+
   // the number of line segments
   public int numberOfSegments()
   { return count; }
@@ -74,32 +99,32 @@ public class FastCollinearPoints {
   { return segments; }
 
   // unit testing
-  public static void main(String[] args) {
-
-      // read the N points from a file
-      In in = new In(args[0]);
-      int N = in.readInt();
-      Point[] points = new Point[N];
-      for (int i = 0; i < N; i++) {
-          int x = in.readInt();
-          int y = in.readInt();
-          points[i] = new Point(x, y);
-      }
-
-      // draw the points
-      StdDraw.show(0);
-      StdDraw.setXscale(0, 32768);
-      StdDraw.setYscale(0, 32768);
-      for (Point p : points) {
-          p.draw();
-      }
-      StdDraw.show();
-
-      // print and draw the line segments
-      BruteCollinearPoints collinear = new BruteCollinearPoints(points);
-      for (LineSegment segment : collinear.segments()) {
-          StdOut.println(segment);
-          segment.draw();
-      }
-  }
+  // public static void main(String[] args) {
+  //
+  //     // read the N points from a file
+  //     In in = new In(args[0]);
+  //     int N = in.readInt();
+  //     Point[] points = new Point[N];
+  //     for (int i = 0; i < N; i++) {
+  //         int x = in.readInt();
+  //         int y = in.readInt();
+  //         points[i] = new Point(x, y);
+  //     }
+  //
+  //     // draw the points
+  //     StdDraw.show(0);
+  //     StdDraw.setXscale(0, 32768);
+  //     StdDraw.setYscale(0, 32768);
+  //     for (Point p : points) {
+  //         p.draw();
+  //     }
+  //     StdDraw.show();
+  //
+  //     // print and draw the line segments
+  //     BruteCollinearPoints collinear = new BruteCollinearPoints(points);
+  //     for (LineSegment segment : collinear.segments()) {
+  //         StdOut.println(segment);
+  //         segment.draw();
+  //     }
+  // }
 }
