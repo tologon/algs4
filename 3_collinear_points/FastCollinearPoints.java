8189ca2855cd1110253fb6a854eceb0b8aec63de
Tologon Eshimkanov <teshimkanov@gmail.com>
2015-10-04 01:34:22 -0400

    Attempt to implement Fast algorithm (without array resizing).

--- 3_collinear_points/FastCollinearPoints.java
+++ 3_collinear_points/FastCollinearPoints.java
@@ -5,7 +5,7 @@ public class FastCollinearPoints {
   // finds all line segments containing 4 or more points
   public FastCollinearPoints(Point[] points) {
     checkPoints(points);
-    segments = new LineSegment[1];
+    segments = new LineSegment[10];
     count = 0;
 
     for (int i = 0; i < points.length; i++) {
@@ -14,12 +14,36 @@ public class FastCollinearPoints {
       Point currentPoint = points[i];
       java.util.Arrays.sort(points, i + 1, points.length,
                             currentPoint.slopeOrder());
+
+      double currentSlope;
+      // continue as long as there are points after current point
+      if (i + 1 <  points.length)
+      { currentSlope = currentPoint.slopeTo(points[i + 1]); }
+      // break when no points exist after current point
+      else
+      { break; }
+      int numOfPoints = 1;
+
       // iterate over the rest of points & find line segments
-      int nextPoint = i + 1;
-      if (nextPoint < points.length) {
-        double currentSlope = currentPoint.slopeTo(points[nextPoint]);
-        while (nextPoint < points.length)
-        { findLineSegments(nextPoint, currentSlope); }
+      for (int k = i + 1; k < points.length; k++) {
+        double newSlope = currentPoint.slopeTo(points[k]);
+        // continue until a truly "new" slope is found
+        if (currentSlope == newSlope)
+        { numOfPoints++; }
+        else {
+          // if 4 or more points, they are collinear
+          if (numOfPoints >= 4) {
+            // TODO implement upSize from Brute Force algorithm
+            Point[] collinearPoints = java.util.Arrays.copyOfRange(points, i, k + 1);
+            java.util.Arrays.sort(collinearPoints);
+            Point first = collinearPoints[0];
+            Point last = collinearPoints[collinearPoints.length - 1];
+            LineSegment s = new LineSegment(first, last);
+            segments[count++] = segment;
+          }
+          currentSlope = newSlope;
+          numOfPoints = 1;
+        }
       }
     }
   }
