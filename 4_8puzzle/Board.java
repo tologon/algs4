a5a13de89745f82534c711f6781f728589d396ea
Tologon Eshimkanov <teshimkanov@gmail.com>
2015-10-10 14:17:56 -0400

    Outline classes for the 4th assignment.

---
+++ 4_8puzzle/Board.java
@@ -0,0 +1,32 @@
+public class Board {
+  // construct a board from an N-by-N array of blocks
+  // (where blocks[i][j] = block in row i, column j)
+  public Board(int[][] blocks)
+
+  // board dimension N
+  public int dimension()
+
+  // number of blocks out of place
+  public int hamming()
+
+  // sum of Manhattan distances between blocks and goal
+  public int manhattan()
+
+  // is this board the goal board?
+  public boolean isGoal()
+
+  // a board that is obtained by exchanging any pair of blocks
+  public Board twin()
+
+  // does this board equal y?
+  public boolean equals(Object y)
+
+  // all neighboring boards
+  public Iterable<Board> neighbors()
+
+  // string representation of this board (in the output format specified below)
+  public String toString()
+
+  // unit testing (not graded)
+  public static void main(String[] args)
+}
