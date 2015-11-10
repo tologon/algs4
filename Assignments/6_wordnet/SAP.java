d1fab074d64080ca4024ca42cea49360e41119a1
Tologon Eshimkanov <teshimkanov@gmail.com>
2015-11-09 20:04:07 -0500

    Start implementing length() method for SAP class.

--- Assignments/6_wordnet/SAP.java
+++ Assignments/6_wordnet/SAP.java
@@ -1,8 +1,10 @@
 import edu.princeton.cs.algs4.Digraph;
 import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.BreadthFirstDirectedPaths;
 
 public class SAP {
-  private Digraph graph;
+  private static final int INFINITY = Integer.MAX_VALUE;
+  private Digraph digraph;
 
   // constructor takes a digraph (not necessarily a DAG)
   public SAP(Digraph G) {
@@ -10,17 +12,36 @@ public class SAP {
       throw new NullPointerException("digraph cannot be null.");
     }
 
-    graph = new Digraph(G.V());
+    digraph = new Digraph(G.V());
     for (int v = 0; v < G.V(); v++) {
       for (int w : G.adj(v)) {
-        graph.addEdge(v, w);
+        digraph.addEdge(v, w);
       }
     }
   }
 
   // length of shortest ancestral path between v and w; -1 if no such path
   public int length(int v, int w) {
-    return -1;
+    BreadthFirstDirectedPaths vPaths =
+      new BreadthFirstDirectedPaths(digraph, v);
+    BreadthFirstDirectedPaths wPaths =
+      new BreadthFirstDirectedPaths(digraph, w);
+
+    int shortestPath = INFINITY;
+    for (int i = 0; i < digraph.V(); i++) {
+      StdOut.println("current i value: " + i);
+      StdOut.println("vPaths.distTo(i): " + vPaths.distTo(i));
+      StdOut.println("wPaths.distTo(i): " + wPaths.distTo(i));
+
+      if (vPaths.hasPathTo(i) && wPaths.hasPathTo(i)
+       && vPaths.distTo(i) + wPaths.distTo(i) < shortestPath) {
+         shortestPath = vPaths.distTo(i) + wPaths.distTo(i);
+         StdOut.println("new short path: " + shortestPath);
+       }
+    }
+
+    if (shortestPath != INFINITY)   return shortestPath;
+    else                            return -1;
   }
 
   // a common ancestor of v and w that
@@ -61,6 +82,7 @@ public class SAP {
     //   int ancestor = sap.ancestor(v, w);
     //   StdOut.printf("length = %d, ancestor = %d\n", length, ancestor);
     // }
+
     Digraph G = new Digraph(6);
     G.addEdge(1, 0);
     G.addEdge(1, 2);
@@ -70,9 +92,10 @@ public class SAP {
     G.addEdge(5, 0);
     StdOut.println("new initial graph:\n" + G);
     SAP sap = new SAP(G);
-    StdOut.println("copied graph in SAP:\n" + sap.graph);
-    G.addEdge(1, 5);
-    StdOut.println("updated initial graph:\n" + G);
-    StdOut.println("unchanged copy graph in SAP:\n" + sap.graph);
+    StdOut.println("length of sap: " + sap.length(1, 5));
+    // StdOut.println("copied graph in SAP:\n" + sap.digraph);
+    // G.addEdge(1, 5);
+    // StdOut.println("updated initial graph:\n" + G);
+    // StdOut.println("unchanged copy graph in SAP:\n" + sap.digraph);
   }
 }
