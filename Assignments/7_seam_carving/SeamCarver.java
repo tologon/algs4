e225f157bb685443a9d14496e79571d791bc3a1f
Tologon Eshimkanov <teshimkanov@gmail.com>
2015-11-22 14:02:38 -0500

    Implement constructor, picture(), width() and height() methods for SeamCarver class.

--- Assignments/7_seam_carving/SeamCarver.java
+++ Assignments/7_seam_carving/SeamCarver.java
@@ -1,18 +1,30 @@
 public class SeamCarver {
+  private Picture picture;
+
   // create a seam carver object based on the given picture
-  public SeamCarver(Picture picture) {}
+  public SeamCarver(Picture picture) {
+    if (picture == null)  throw new NullPointerException();
+
+    this.picture = new Picture(picture);
+  }
 
   // current picture
-  public Picture picture() {}
+  public Picture picture()
+  { return picture; }
 
   // width of current picture
-  public int width() {}
+  public int width()
+  { return picture.width(); }
 
   // height of current picture
-  public int height() {}
+  public int height()
+  { return picture.height(); }
 
   // energy of pixel at column x and row y
-  public double energy(int x, int y) {}
+  public double energy(int x, int y) {
+    if (x < 0 || x >= width() || y < 0 || y >= height())
+    { throw new IndexOutOfBoundsException("x and/or y are outside of range"); }
+  }
 
   // sequence of indices for horizontal seam
   public int[] findHorizontalSeam() {}
@@ -21,8 +33,16 @@ public class SeamCarver {
   public int[] findVerticalSeam() {}
 
   // remove horizontal seam from current picture
-  public void removeHorizontalSeam(int[] seam) {}
+  public void removeHorizontalSeam(int[] seam) {
+    if (seam == null)   throw new NullPointerException("seam cannot be null.");
+    if (height() <= 1)          throw new IllegalArgumentException();
+    if (seam.length != width()) throw new IllegalArgumentException();
+  }
 
   // remove vertical seam from current picture
-  public void removeVerticalSeam(int[] seam) {}
+  public void removeVerticalSeam(int[] seam) {
+    if (seam == null)   throw new NullPointerException("seam cannot be null.");
+    if (width() <= 1)             throw new IllegalArgumentException();
+    if (seam.length != height())  throw new IllegalArgumentException();
+  }
 }
